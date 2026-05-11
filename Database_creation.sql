8816629fc4ff5aff82d80819338e81c066ca6344
PavanSurisetti <pavan1898235@gmail.com>
2026-05-11 20:57:54 +0530

    Database Creation

---
+++ Database_creation.sql
@@ -0,0 +1,5 @@
+-- to create,use,view and drop databases
+create database db; -- to create a database
+show databases; -- we can view all databases
+use db; -- to use a specific database
+drop database db; -- we can delete the existing database
