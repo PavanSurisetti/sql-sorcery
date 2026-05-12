193de738845be8d45820640c862387a1fde3842d
PavanSurisetti <161280590+PavanSurisetti@users.noreply.github.com>
2026-05-12 13:44:22 +0530

    NOT NULL Constraint

--- Defining_NOT_NULL_Constraint.sql
+++ Defining_NOT_NULL_Constraint.sql
@@ -4,4 +4,4 @@ create table age_table(age int NOT NULL);
 describe age_table;
 insert into age_table values(1),(2);
 select * from age_table;
--- insert into age_table values(); if try this you wil get ann error
+-- insert into age_table values(); if you try this, you will get an error
