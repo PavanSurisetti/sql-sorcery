e376745b32d3340849413f6d2dd2c25f476e1d5e
PavanSurisetti <pavan1898235@gmail.com>
2026-06-02 21:24:27 +0530

    Group By Command In SQL

---
+++ GROUP_BY_Command.sql
@@ -0,0 +1,12 @@
+-- using GROUP BY 
+use db;
+create table emp(id int primary key,name varchar(30) NOT NULL,age int not null,dept varchar(30) default 'NTD',salary float);
+describe emp;
+insert into emp values(1,'Ganesha',30,'CSE',50000),(2,'Satya',28,'CSE',45000),(3,'Alexa',50,'CSE',80000),(4,'Raju',56,'IT',30000),(5,'Maeve',30,'IT',23000),(6,'Rajesh',63,'MEC',80000),(7,'satish',50,'MEC',75000);
+select *from emp;
+-- finding no of employees in each branch
+select dept, count(dept) as no_of_employees from emp group by dept;
+-- finding avg age of employee in each department
+select dept,avg(age) as average_age from emp group by dept;
+-- finding total funding to each department 
+select dept,sum(salary) as total_fund from emp group by dept;
