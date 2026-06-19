370d40c4cbadbc6c62e9a76a2e20aedf0dfd0c89
PavanSurisetti <pavan1898235@gmail.com>
2026-06-19 15:27:24 +0530

    INNER JOIN

---
+++ INNER_JOIN.sql
@@ -0,0 +1,12 @@
+-- creating two tables for performing Inner join
+use db;
+-- creating a table for student
+create table student(Roll_No int primary key,name varchar(30),age int);
+insert into student values(1,'Ram',20),(2,'Sam',21),(3,'John',22),(4,'Ravi',23);
+select *from student;
+-- creating another table for course
+create table studentCourse(Roll_No int ,course_id varchar(30) primary key);
+describe studentCourse;
+insert into studentCourse values(1,'C101'),(2,'C102'),(4,'C103'),(5,'C104');
+-- performing Inner Join operation
+select student.Roll_No,student.name,studentCourse.course_id from student INNER JOIN studentCourse on student.Roll_No=studentCourse.Roll_No;
